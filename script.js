d84dfd50fc63867a5cb886525f34990bf8537d0c
SiGGy3 <justinjwsig@gmail.com>
2023-03-31 14:04:21 -0700

    added temporary 'onclick' for animation

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var character = 
-document.getElementById("character");
-var block = document.getElementById("bloc")
+var dinosaur = 
+document.getElementById("dinosaur");
+var block = document.getElementById("block");
+
+// add the animation to the dinosaur by adding 'class'
+function jump() {
+    if(dinosaur.classList != "animate"){
+    dinosaur.classList.add("animate"); 
+    }
+    setTimeout(function(){
+        dinosaur.classList.remove("animate")
+    },500);
+}
