e27b61635b6ac8cb9a4fcafa68c32575ee3d44fe
SiGGy3 <justinjwsig@gmail.com>
2023-04-01 11:47:40 -0700

    game restarts on "ok" press

---
+++ README.md
@@ -0,0 +1,2 @@
+Welcome to a clone of the Chrome dinosaur browser game! Your objective is to jump over as many cacti as you can without getting hit. If you get hit, then you lose!
+sprites taken from https://www.spriters-resource.com/browser_games/googledinosaurrungame/sheet/78171/
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var dinosaur = 
 document.getElementById("dinosaur");
-var block = document.getElementById("block");
+var cactus = document.getElementById("cactus");
+var isAlive = true
+
+
+
+let lastTime 
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+    
+    
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+window.requestAnimationFrame(update)
 
 // add the animation to the dinosaur by adding 'class'
 function jump() {
@@ -11,3 +30,21 @@ function jump() {
         dinosaur.classList.remove("animate")
     },500);
 }
+// WIN CONDITION & HIT DETECTION
+var checkDeath = setInterval(function(){
+    if (isAlive == false) return;
+    var dinosaurTop = 
+    parseInt(window.getComputedStyle(dinosaur).
+    getPropertyValue("top"));
+    var  cactusLeft = 
+    parseInt(window.getComputedStyle(cactus).
+    getPropertyValue("left"));
+    if(cactusLeft<70 && cactusLeft>0 && dinosaurTop>130){
+        cactus.style.animation = "none";
+        cactus.style.display = "none";
+        alert("You Lost!");
+        // game resets on "ok"
+        isAlive = false;
+        location.reload();
+    }
+},10);
